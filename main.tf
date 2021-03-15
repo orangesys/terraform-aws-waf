3a8fe306e1f8c75357786c44bb798733bacf8622
xucheng <jobxucheng@gmail.com>
2021-03-15 13:51:20 +0900

    add new

--- main.tf
+++ main.tf
@@ -1,7 +1,61 @@
 resource "aws_wafv2_ip_set" "blacklist" {
-  name               = "blacklist"
+  name               = "${var.app_name}-blacklist-${var.env}"
   description        = "Blacklist IP set"
-  scope              = "CLOUDFRONT"
+  scope              = var.is_cloudfront ? "CLOUDFRONT" : "REGIONAL"
   ip_address_version = "IPV4"
   addresses          = var.blacklist
+  tags               = var.tags
+}
+
+resource "aws_wafv2_web_acl" "acl" {
+  name        = "${var.app_name}-web-acls"
+  description = "Web ACLs"
+  scope       = var.is_cloudfront ? "CLOUDFRONT" : "REGIONAL"
+
+  default_action {
+    block {}
+  }
+
+  rule {
+    name     = "default"
+    priority = 1
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesCommonRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = false
+      metric_name                = "${var.app_name}-${var.env}-default-rule-metric"
+      sampled_requests_enabled   = false
+    }
+  }
+
+  rule {
+    name     = "blocklist"
+    priority = 2
+
+    statement {
+      ip_set_reference_statement {
+        arn = aws_wafv2_ip_set.blacklist.arn
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = false
+      metric_name                = "${var.app_name}-${var.env}-blocklist-rule-metric"
+      sampled_requests_enabled   = false
+    }
+  }
+
+  tags = tags
+
+  visibility_config {
+    cloudwatch_metrics_enabled = false
+    metric_name                =  "${var.app_name}-${var.env}-metric"
+    sampled_requests_enabled   = false
+  }
 }
--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "blacklist" {
   description = "blacklist"
-  type        = list
+  type        = list(any)
   default     = []
 }
 
@@ -8,4 +8,21 @@ variable "env" {
   description = "enviornment variable"
   type        = string
   default     = "test"
+}
+
+variable "app_name" {
+  description = "application name"
+  type        = string
+}
+
+variable "is_cloudfront" {
+  description = "whether create for cloudfront"
+  type        = bool
+  default     = false
+}
+
+variable "tags" {
+  description = "A mapping of tags to assign to the WAF"
+  type        = map(string)
+  default     = {}
 }
